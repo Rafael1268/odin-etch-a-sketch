d71a8d7b54eb7a86ecd4b1a9845a244a5b989c55
RafaelB <rafaelboschmamail@gmail.com>
2022-05-12 23:01:09 +0200

    Drawing system finished

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
 const drawingSpace = document.querySelector('.drawingSpace');
+const changeGridSize = document.querySelector('#changeGridSize');
+changeGridSize.addEventListener('click', () => gridSize());
+document.addEventListener('mousedown', () => mouseDown = true);
+document.addEventListener('mouseup', () => mouseDown = false);
+let defaultColor = "black";
+let mouseDown = false;
 
+createGrid(16);
+
+// Creates the grid
 function createGrid(gridSize) {
     const gridTotal = gridSize * gridSize;
     drawingSpace.setAttribute('style', `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-columns: repeat(${gridSize}, 1fr)`);
-    for (i = 0; i < gridTotal; i++) {
-        const content = document.createElement('div');
-        content.classList.add("div" + (i + 1))
-        drawingSpace.appendChild(content);
+    for (let i = 0; i < gridTotal; i++) {
+        editGrid(i);
     }
 }
 
-createGrid(16);
+// Edits the div for each grid
+function editGrid(i) {
+    const content = document.createElement('div');
+    content.classList.add('grid');
+    content.id = `div${i}`;
+    content.draggable = false;
+    drawingSpace.appendChild(content);
+    content.addEventListener('mousemove', () => changeColor(content));
+}
+
+// Changes the color of the grid when hovering over
+function changeColor(grid) {
+    if (mouseDown === false) return;
+    const currentGrid = document.getElementById(grid.id);
+    currentGrid.setAttribute('style', `background-color: ${defaultColor};`)
+}
+
+// Changes the grid size
+function gridSize() {
+    const allGrids = document.querySelectorAll('div.grid');
+    const newSize = prompt("How large should the grid be?");
+    if (newSize > 64) return alert("That's too big! Max is 64.");
+    allGrids.forEach(grid => drawingSpace.removeChild(grid));
+    createGrid(newSize);
+}
