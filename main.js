b6885b3ab5e682ba6be8410987503ffce52a4a97
RafaelB <rafaelboschmamail@gmail.com>
2022-05-13 11:51:13 +0200

    Added rainbow mode

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ const drawingSpace = document.querySelector('.drawingSpace');
 const changeGridSize = document.querySelector('#changeGridSize');
 const colorPicker = document.querySelector('#colorPicker');
 const eraseGridBtn = document.querySelector('#eraseGrid');
+const rainbowModeBtn = document.querySelector('#rainbowMode');
 changeGridSize.addEventListener('click', () => gridSize());
 eraseGridBtn.addEventListener('click', () => eraseGrid());
 document.addEventListener('mousedown', () => mouseDown = true);
 document.addEventListener('mouseup', () => mouseDown = false);
+let rainbowColors = ["#ff3232", "#FF9A32", "#FFFF32", "#32FF3D", "#32E9FF", "#3632FF", "#D432FF"];
 let mouseDown = false;
 let currentGridSize = 16;
+let rainbowMode = false;
+rainbowModeBtn.addEventListener('click', () => rainbowModeToggle());
 
 createGrid(16);
 
@@ -27,14 +31,19 @@ function editGrid(i) {
     content.id = `div${i}`;
     content.draggable = false;
     drawingSpace.appendChild(content);
-    content.addEventListener('mousemove', () => changeColor(content));
+    content.addEventListener('mouseover', () => changeColor(content));
 }
 
 // Changes the color of the grid when hovering over
 function changeColor(grid) {
     if (mouseDown === false) return;
     const currentGrid = document.getElementById(grid.id);
-    currentGrid.setAttribute('style', `background-color: ${colorPicker.value};`)
+    const random = Math.floor(Math.random() * rainbowColors.length);
+    if (rainbowMode === true) {
+        currentGrid.setAttribute('style', `background-color: ${rainbowColors[random]};`);
+    } else {
+        currentGrid.setAttribute('style', `background-color: ${colorPicker.value};`);
+    }
 }
 
 // Changes the grid size
@@ -42,7 +51,7 @@ function gridSize() {
     const allGrids = document.querySelectorAll('div.grid');
     const newSize = prompt("How large should the grid be?");
     const newSizeNumber = Number(newSize);
-    if(isNaN(newSizeNumber) || newSizeNumber < 1) return console.log("1");
+    if(isNaN(newSizeNumber) || newSizeNumber < 1) return;
     currentGridSize = newSizeNumber;
     allGrids.forEach(grid => drawingSpace.removeChild(grid));
     createGrid(newSizeNumber);
@@ -53,4 +62,15 @@ function eraseGrid() {
     const allGrids = document.querySelectorAll('div.grid');
     allGrids.forEach(grid => drawingSpace.removeChild(grid));
     createGrid(currentGridSize);
+}
+
+// Toggles rainbow mode
+function rainbowModeToggle() {
+    if (rainbowMode === false) {
+        rainbowModeBtn.style.color = '#5eff00';
+        rainbowMode = true;
+    } else {
+        rainbowModeBtn.style.color = 'black';
+        rainbowMode = false;
+    }
 }
