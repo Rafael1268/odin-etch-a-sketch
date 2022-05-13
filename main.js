00eb65e0cef03c0785236ce51ce1f4c9f8bb8b05
RafaelB <rafaelboschmamail@gmail.com>
2022-05-13 11:17:21 +0200

    Stylized the page, added "ERASE GRID" button

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const drawingSpace = document.querySelector('.drawingSpace');
 const changeGridSize = document.querySelector('#changeGridSize');
 const colorPicker = document.querySelector('#colorPicker');
+const eraseGridBtn = document.querySelector('#eraseGrid');
 changeGridSize.addEventListener('click', () => gridSize());
+eraseGridBtn.addEventListener('click', () => eraseGrid());
 document.addEventListener('mousedown', () => mouseDown = true);
 document.addEventListener('mouseup', () => mouseDown = false);
 let mouseDown = false;
+let currentGridSize = 16;
 
 createGrid(16);
 
@@ -38,8 +41,16 @@ function changeColor(grid) {
 function gridSize() {
     const allGrids = document.querySelectorAll('div.grid');
     const newSize = prompt("How large should the grid be?");
-    if (newSize > 64) return alert("That's too big! Max is 64.");
-    if (!newSize) return;
+    const newSizeNumber = Number(newSize);
+    if(isNaN(newSizeNumber) || newSizeNumber < 1) return console.log("1");
+    currentGridSize = newSizeNumber;
     allGrids.forEach(grid => drawingSpace.removeChild(grid));
-    createGrid(newSize);
+    createGrid(newSizeNumber);
+}
+
+// Erases the grid
+function eraseGrid() {
+    const allGrids = document.querySelectorAll('div.grid');
+    allGrids.forEach(grid => drawingSpace.removeChild(grid));
+    createGrid(currentGridSize);
 }
